c879ee6fff9518908c5b5f11162f3bd7f9ee0be1
Raiszo <E05.sek@gmail.com>
2018-09-15 18:40:53 -0500

    :dagger: adding build_mlp

--- policy.py
+++ policy.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-def build_mlp(
+class Policy(
         input_placeholder,
         output_size,
         scope,
@@ -8,11 +8,23 @@ def build_mlp(
         size=64,
         activation=tf.tanh
         ):
-    with tf.variable_scope(scope):
-        y = input_placeholder
-        
-        for i in range(n_layers):
-            y = tf.layers.dense(y, size, activation=activation, use_bias=True)
+    def __init__(self, name, state):
+        with tf.variable_scope(name):
+            state = input_placeholder
+            
+            with tf.variable_scope('vf'):
+                y = state
+                for i in range(n_layers):
+                    y = tf.layers.dense(y, size, activation=activation, use_bias=True)
 
-        y = tf.layers.dense(y,output_size, use_bias=True)
-        return y
+                self.vpred = tf.layers.dense(y,output_size, use_bias=True)
+
+            with tf.variable_scope('pol'):
+                y = state
+                for i in range(n_layers):
+                    y = tf.layers.dense(y, size, activation=activation, use_bias=True)
+
+                self.vpred = tf.layers.dense(y, 1, use_bias=True)
+
+    def act(self):
+        
