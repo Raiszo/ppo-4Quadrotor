9b7428b1b1f171887af23d02161aadfb5a576f88
Raiszo <E05.sek@gmail.com>
2018-09-16 22:47:09 -0500

    :dagger: testing a dummy agent

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import gym
 import numpy as np
 from ppo import rollouts_generator
-
-class random_policy:
-    def __init__(self, env):
-        self.action = env.action_space.sample
-
-    def act(self):
-        return self.action(), np.random.randn()
+from policy import Policy
+import tensorflow as tf
 
 def main():
     env = gym.make('Pendulum-v0')
-    pi = random_policy(env)
+
+    obs_ph = tf.placeholder(tf.float32, shape=[None, env.observation_space.shape[0]])
+    ac_dim = env.action_space.shape[0]
+    
+    pi = Policy('veronika', obs_ph, ac_dim, 2)
+    # generator = rollouts_generator(pi, env, 2048)
+
+    # gen = generator.__next__()
 
     
-    generator = rollouts_generator(pi, env, 2048)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        obs = env.reset()
 
-    gen = generator.__next__()
-    
+        for i in range(10):
+            ac, v = pi.act(sess, obs)
+            obs, rew, done, _ = env.step(ac)
 
 if __name__ == '__main__':
     main()
--- policy.py
+++ policy.py
@@ -1,30 +1,42 @@
 import tensorflow as tf
 
-class Policy(
-        input_placeholder,
-        output_size,
-        scope,
-        n_layers=2,
-        size=64,
-        activation=tf.tanh
-        ):
-    def __init__(self, name, state):
+def build_mlp(n_layers, input_placeholder, output_size):
+    y = input_placeholder
+    for i in range(n_layers):
+        y = tf.layers.dense(y, size, activation=tf.tanh, use_bias=True)
+
+    return tf.layers.dense(y, output_size, use_bias=True)
+
+class Policy:
+    def __init__(self, name, state_placeholder, action_dim, n_layers, continuos=True):
+        self.state = state_placeholder
+        activation = tf.tanh
+        
         with tf.variable_scope(name):
-            state = input_placeholder
             
             with tf.variable_scope('vf'):
-                y = state
+                y = self.state
                 for i in range(n_layers):
-                    y = tf.layers.dense(y, size, activation=activation, use_bias=True)
-
-                self.vpred = tf.layers.dense(y,output_size, use_bias=True)
-
-            with tf.variable_scope('pol'):
-                y = state
-                for i in range(n_layers):
-                    y = tf.layers.dense(y, size, activation=activation, use_bias=True)
+                    y = tf.layers.dense(y, 64, activation=activation, use_bias=True)
 
                 self.vpred = tf.layers.dense(y, 1, use_bias=True)
 
+            with tf.variable_scope('pol'):
+                y = self.state
+                for i in range(n_layers):
+                    y = tf.layers.dense(y, 64, activation=activation, use_bias=True)
+
+                self.action = tf.layers.dense(y, action_dim, use_bias=True)
+
+    def act(self, sess, obs):
+        ac, v = sess.run([self.action, self.vpred], feed_dict={self.state: obs[None]})
+        return ac[0], v[0]
+
+
+class Random_policy:
+    def __init__(self, env):
+        self.action = env.action_space.sample
+
     def act(self):
-        
+        return self.action(), np.random.randn()
+
