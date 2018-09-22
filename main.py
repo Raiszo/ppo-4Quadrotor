79054d03704b4f8640d7687e58a449fb9a1b4a88
Raiszo <E05.sek@gmail.com>
2018-09-22 15:04:49 -0500

    :dagger: testing on the Pendulum-v0 :''''v

--- main.py
+++ main.py
@@ -12,25 +12,54 @@ def main():
     
     gamma, lam = 0.99, 0.95
     std = 0.2
+    learning_rate = 0.05
 
+    # Sampled variables
     ob_no = tf.placeholder(tf.float32, shape=[None, env.observation_space.shape[0]])
     ac_na = tf.placeholder(tf.float32, shape=[None, env.action_space.shape[0]])
     adv_n = tf.placeholder(tf.float32, shape=[None])
+    val_n = tf.placeholder(tf.float32, shape=[None])
+
+    # Target Value function
+    t_val = tf.placeholder(tf.float32, shape=[None])
     
     pi = Policy('veronika', ob_no, ac_dim, n_layers=2)
 
     
-    # Sampling operation
-    sy_mean = pi.action
-    sy_logprob_n = (ob_no - sy_mean) / std**2
+    # Gaussian policy loss operations
+    mean_na = pi.action
+    logprob_n = (ac_na - mean_na) / std**2
+    pg_loss = tf.reduce_mean(logprob_n)
 
+    # Value function loss operations
+    b_loss = tf.reduce_mean(tf.losses.mean_squared_error(labels=t_val, predictions=val_n))
+    
+
+    loss = pg_loss + b_loss
+    update_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+
+    init = tf.global_variables_initializer()
     # gen = generator.__next__()
     with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
+        sess.run(init)
         
         generator = rollouts_generator(sess, pi, env, 202)
-        seg = generator.__next__()
-        add_vtarg_adv(seg, lam, gamma)
+
+        for _ in range(100):
+            seg = generator.__next__()
+            add_vtarg_adv(seg, lam, gamma)
+
+            feed_dict = {
+                ob_no: seg["ob"],
+                ac_na: seg["ac"],
+                adv_n: seg["adv"],
+                val_n: seg["vpred"],
+                t_val: seg["vtarg"]
+            }
+
+            _loss, _ = sess.run([loss, update_op], feed_dict=feed_dict)
+
+            print(_loss)
 
         
 
