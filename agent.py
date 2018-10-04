f8ec1a041b2eed97a7532a7e2d3c4d4a2f2790c2
Raiszo <E05.sek@gmail.com>
2018-10-04 18:37:27 -0500

    :dagger: now using tf normal distribution for continuous action

--- agent.py
+++ agent.py
@@ -1,36 +1,51 @@
 import tensorflow as tf
-from network import Network
+from network import build_mlp
 from ppo import multi_normal
 
 class Agent:
-    def __init__(self, name, state_placeholder, action_dim, continuous, n_layers, std=0.5):
+    def __init__(self, state_placeholder, action_dim, continuous, n_layers):
         self.continuous = continuous
         self.state = state_placeholder
-        self.std = tf.constant(std)
 
 
-        self.pi = Network(2, state_placeholder, action_dim, 'policy')
-        self.old_pi = Network(2, state_placeholder, action_dim, 'old_policy')
-        self.v_func = Network(2, state_placeholder, action_dim, 'value_estimator')
+        with tf.variable_scope('policy'):
+            self.pi = build_mlp(2, state_placeholder, action_dim)
+            logstd = tf.get_variable(name='logstd', shape=[1, action_dim],
+                                     initializer=tf.zeros_initializer())
+            self.std = tf.zeros_like(self.pi) + tf.exp(logstd)
+            self.dist = tf.distributions.Normal(loc=self.pi, scale=self.std)
+            pi_scope = tf.get_variable_scope().name
+            pi_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, pi_scope)
+
+        self.vars = pi_vars
+            
+        with tf.variable_scope('old_policy'):
+            self.old_pi = build_mlp(2, state_placeholder, action_dim)
+            old_logstd = tf.get_variable(name='logstd', shape=[1, action_dim],
+                                         initializer=tf.zeros_initializer())
+            self.old_std = tf.zeros_like(self.old_pi) + tf.exp(old_logstd)
+            self.old_dist = tf.distributions.Normal(loc=self.old_pi, scale=self.old_std)
+            old_pi_scope = tf.get_variable_scope().name
+            old_pi_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, old_pi_scope)
+
+        with tf.variable_scope('value'):
+            self.vpred = build_mlp(1, state_placeholder, action_dim)
+            # vpred_vars = tf.get_variable_scope().name
         
-        self.pi_vars = self.pi.get_vars()
-        self.old_pi_vars = self.old_pi.get_vars()
-
-        self.sample_action = multi_normal(self.pi.logits, self.std) \
+        self.sample_action = self.dist.sample(1) \
                 if continuous \
-                   else tf.multinomial(self.pi.logits - tf.reduce_max(self.pi.logits, axis=1, keepdims=True), 1)
-        self.vpred = self.v_func.logits
+                   else tf.multinomial(self.pi - tf.reduce_max(self.pi, axis=1, keepdims=True), 1)
 
         with tf.variable_scope('assign_op'):
             self.assign_ops = []
-            for v_old, v in zip(self.old_pi_vars, self.pi_vars):
+            for v_old, v in zip(old_pi_vars, pi_vars):
                 self.assign_ops.append(tf.assign(v_old, v))
 
         
-            
+        
     def act(self, sess, obs):
         ac, v = sess.run([self.sample_action, self.vpred], feed_dict={self.state: obs[None]})
-        return(ac[0], v[0]) if self.continuous else (ac[0][0], v[0][0])
+        return(ac[0][0], v[0][0]) if self.continuous else (ac[0][0], v[0][0])
 
     def save_policy(self, sess):
         return sess.run(self.assign_ops)
--- network.py
+++ network.py
@@ -1,15 +1,8 @@
 import tensorflow as tf
 
-class Network:
-    def __init__(self, n_layers, input_placeholder, output_size, scope, size=64):
-        with tf.variable_scope(scope):
-            y = input_placeholder
-            for i in range(n_layers):
-                y = tf.layers.dense(y, size, activation=tf.tanh, use_bias=True)
+def build_mlp(n_layers, input_placeholder, output_size, size=64):
+    y = input_placeholder
+    for i in range(n_layers):
+        y = tf.layers.dense(y, size, activation=tf.tanh, use_bias=True)
 
-            self.logits = tf.layers.dense(y, output_size, use_bias=True)
-            self.scope = tf.get_variable_scope().name
-
-
-    def get_vars(self):
-        return tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, self.scope)
+    return tf.layers.dense(y, output_size, use_bias=True)
