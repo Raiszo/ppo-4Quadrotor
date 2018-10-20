899bdac74f5dfb1b076b9da53cac278637af7b81
Raiszo <E05.sek@gmail.com>
2018-10-19 20:02:01 -0500

    :dagger: adding logger, not tested yey

--- continuous.py
+++ continuous.py
@@ -4,42 +4,33 @@ from ppo import rollouts_generator, add_vtarg_adv, render, Sensei
 from agent import Agent
 import tensorflow as tf
 import gym_pendrogone
-import logz
+import Logger from logger_z
 
 tf.set_random_seed(0)
 
 two_pi = np.sqrt(0.5 / np.pi)
 
 def experiment(log_dir, exp_name, env_name, num_iterations, sample_horizon,
-               gamma, lam, learning_rate, epsilon, batch_size):
+               gamma, lam, learning_rate, epsilon, epochs, batch_size):
     env = gym.make(env_name)
-
-    logger = logz
-    logger.configure_output_dir(log_dir)
-
-    
-    
-
-def main():
-    # env = gym.make('MountainCarContinuous-v0')
-    env = gym.make('Pendrogone-v0')
-    # env = gym.make('CartPole-v1')
-
     continuous = isinstance(env.action_space, gym.spaces.Box)
     ob_dim = env.observation_space.shape[0]
     ac_dim = env.action_space.shape[0] if continuous else env.action_space.n
 
-    num_iterations = 400
-    sample_horizon = 2048
-    # num_iterations = 1
-    # sample_horizon = 5
-    gamma, lam = 0.99, 0.95
+    
+    ### Logger configuration
+    logger = logz(log_dir)
+    args = inspect.getargspec(self.__init__)[0]
+    locals_ = locals()
+    params = {k: locals_[k] if k in locals_ else None for k in args}
+    logger.save_params(params)
+
 
     vero = Agent(continuous, ob_dim, ac_dim, n_layers=2)
     regina = Sensei(vero, continuous, ob_dim, ac_dim,
-                    num_epochs=10, batch_size=64,
+                    num_epochs, batch_size,
                     # num_epochs=1, batch_size=5,
-                    learning_rate=5e-3, epsilon=0.2)
+                    learning_rate, epsilon)
 
     init = tf.global_variables_initializer()
     # gen = generator.__next__()
@@ -59,13 +50,70 @@ def main():
             regina.train_samples(sess, seg["ob"], seg["ac"], adv, seg["vtarg"], seg["log_probs"])
 
             rewards = np.array(seg["ep_rets"])
-            # print(rewards)
 
             if i % 10 == 0 or i == num_iterations-1:
+                mean, std = rewards.mean(), rewards.std()
+                
+                logger.log_tabular("AverageReturn", mean)
+                logger.log_tabular("StdReturn", std)
+                logger.dump_tabular()
                 print('Iteration {0:3d}: with average rewards {1:5.3f} and std {2:5.2f}'
-                      .format(i, rewards.mean(), rewards.std()))
+                      .format(i, mean, std))
 
         render(sess, vero, env)
+    
+def main():
+    experiment('experimentito_01', 'experimentito', 'CartPole-v1', num_iterations=1, sample_horizon=5,
+               gamma=0.99, lam=0.95, learning_rate=5e-3, epsilon=0.2, epochs=1, batch_size=5)
+    
+    
+
+# def main():
+#     # env = gym.make('MountainCarContinuous-v0')
+#     env = gym.make('Pendrogone-v0')
+#     # env = gym.make('CartPole-v1')
+
+#     continuous = isinstance(env.action_space, gym.spaces.Box)
+#     ob_dim = env.observation_space.shape[0]
+#     ac_dim = env.action_space.shape[0] if continuous else env.action_space.n
+
+#     num_iterations = 400
+#     sample_horizon = 2048
+#     # num_iterations = 1
+#     # sample_horizon = 5
+#     gamma, lam = 0.99, 0.95
+
+#     vero = Agent(continuous, ob_dim, ac_dim, n_layers=2)
+#     regina = Sensei(vero, continuous, ob_dim, ac_dim,
+#                     num_epochs=10, batch_size=64,
+#                     # num_epochs=1, batch_size=5,
+#                     learning_rate=5e-3, epsilon=0.2)
+
+#     init = tf.global_variables_initializer()
+#     # gen = generator.__next__()
+#     with tf.Session() as sess:
+#         sess.run(init)
+        
+#         generator = rollouts_generator(sess, vero, env, sample_horizon)
+
+#         for i in range(num_iterations):
+#             seg = generator.__next__()
+#             add_vtarg_adv(seg, lam, gamma)
+
+#             adv = seg["adv"]
+#             adv = (adv - adv.mean()) / (adv.std() + 1e-8)
+#             adv = adv[:, None]
+
+#             regina.train_samples(sess, seg["ob"], seg["ac"], adv, seg["vtarg"], seg["log_probs"])
+
+#             rewards = np.array(seg["ep_rets"])
+#             # print(rewards)
+
+#             if i % 10 == 0 or i == num_iterations-1:
+#                 print('Iteration {0:3d}: with average rewards {1:5.3f} and std {2:5.2f}'
+#                       .format(i, rewards.mean(), rewards.std()))
+
+#         render(sess, vero, env)
         
 
 if __name__ == '__main__':
---
+++ logger.py
@@ -0,0 +1,92 @@
+import json
+
+"""
+
+Some simple logging functionality, inspired by rllab's logging.
+Assumes that each diagnostic gets logged each iteration
+
+Call logz.configure_output_dir() to start logging to a 
+tab-separated-values file (some_folder_name/log.txt)
+
+To load the learning curves, you can do, for example
+
+A = np.genfromtxt('/tmp/expt_1468984536/log.txt',delimiter='\t',dtype=None, names=True)
+A['EpRewMean']
+
+"""
+
+import os.path as osp, shutil, time, atexit, os, subprocess
+import pickle
+# import tensorflow as tf
+
+color2num = dict(
+    gray=30,
+    red=31,
+    green=32,
+    yellow=33,
+    blue=34,
+    magenta=35,
+    cyan=36,
+    white=37,
+    crimson=38
+)
+
+class Logger:
+    def __init__(self, output_dir):
+        assert not osp.exists(output_dir), "Log dir %s already exists! Delete it first or use a different dir"%output_dir
+
+        self.output_dir = output_dir
+        os.makedirs(output_dir)
+        
+        self.first_row = True
+        self.log_headers = []
+        self.log_current_row = {}
+
+        
+        self.output_file = open(osp.join(output_dir, "log.txt"), 'w')
+        atexit.register(self.output_file.close)
+        print(colorize("Logging data to %s"%self.output_file.name, 'green', bold=True))
+
+    def save_params(self, params):
+        with open(osp.join(self.output_dir, "params.json"), 'w') as out:
+            out.write(json.dumps(params, separators=(',\n','\t:\t'), sort_keys=True))
+
+    def log_tabular(self, key, val):
+        """
+        Log a value of some diagnostic
+        Call this once for each diagnostic quantity, each iteration
+        """
+        if self.first_row:
+            self.log_headers.append(key)
+        else:
+            assert key in self.log_headers, "Trying to introduce a new key %s that you didn't include in the first iteration"%key
+            assert key not in self.log_current_row, "You already set %s this iteration. Maybe you forgot to call dump_tabular()"%key
+            self.log_current_row[key] = val
+
+    # def dump_tabular(print_console):
+    def dump_tabular(self):
+        """
+        Write all of the diagnostics from the current iteration
+        """
+        vals = []
+        key_lens = [len(key) for key in self.log_headers]
+        max_key_len = max(15,max(key_lens))
+        keystr = '%'+'%d'%max_key_len
+        fmt = "| " + keystr + "s | %15s |"
+        n_slashes = 22 + max_key_len
+        
+        for key in self.log_headers:
+            val = self.log_current_row.get(key, "")
+            if hasattr(val, "__float__"): valstr = "%8.3g"%val
+            else: valstr = val
+            vals.append(val)
+
+        if self.output_file is not None:
+            if self.first_row:
+                self.output_file.write("\t".join(self.log_headers))
+                self.output_file.write("\n")
+            self.output_file.write("\t".join(map(str, vals)))
+            self.output_file.write("\n")
+            self.output_file.flush()
+        self.log_current_row.clear()
+        self.first_row=False
