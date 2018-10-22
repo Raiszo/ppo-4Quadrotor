34b5a544342d8fb165e590a95eec2b12817e8202
Raiszo <E05.sek@gmail.com>
2018-10-22 01:48:48 -0500

    :dagger: session configuration for multiprocessing

---
+++ utils.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+def get_session():
+    tf_config = tf.ConfigProto(
+        inter_op_parallelism_threads=1,
+        intra_op_parallelism_threads=1
+    )
+
+    return tf.Session(config=tf_config)
+    
