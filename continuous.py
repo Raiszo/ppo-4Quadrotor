605aed9e6205c72e2e9337ca894021ead8899957
Raiszo <E05.sek@gmail.com>
2018-10-22 01:49:52 -0500

    :dagger: experiment conf with multiple seeds with multiprocessing

--- continuous.py
+++ continuous.py
@@ -1,45 +1,68 @@
 import gym
 import os, math, time, inspect
+from multiprocessing import Process
 import numpy as np
 from ppo import rollouts_generator, add_vtarg_adv, render, Sensei
+from utils import get_session
 from agent import Agent
 import tensorflow as tf
 # import gym_pendrogone
 from logger import Logger as Logz
 
+def experiment(**args):
+    initial_seed = 0
+    n_experiments = 4
+    # print(args)
+    
+    if not(os.path.exists('experiments')):
+        os.makedirs('experiments')
+    
+    experiment_dir = args["exp_name"] + '_' + args["env_name"] + '_' + time.strftime("%d-%m-%Y_%H-%M-%S")
+    experiment_dir = os.path.join('experiments', experiment_dir)
+    
+    if not(os.path.exists(experiment_dir)):
+        os.makedirs(experiment_dir)
+        
+    # args = inspect.getargspec(experiment)[0]
+    # locals_ = locals()
+    # params = {k: locals_[k] if k in locals_ else None for k in args}
+    Logz.save_params(experiment_dir, args)
 
-tf.set_random_seed(0)
+    processes = []
+    for e in range(n_experiments):
+        seed = initial_seed + 10*e
+        log_dir = os.path.join(experiment_dir,'%d'%seed)
+        def train_func():
+            train_process(log_dir=log_dir, seed=seed, **args)
 
-two_pi = np.sqrt(0.5 / np.pi)
+        p = Process(target=train_func, args=tuple())
+        p.start()
+        processes.append(p)
 
-def experiment(log_dir, exp_name, env_name, num_iterations, sample_horizon,
-               gamma, lam, learning_rate, epsilon, epochs, batch_size):
+    for i in processes:
+        p.join()
+    print('finished trainning :3')
+    
+    
 
+def train_process(log_dir, exp_name, env_name, num_iterations, sample_horizon,
+                  gamma, lam, learning_rate, epsilon, epochs, batch_size, seed):
+    tf.set_random_seed(seed)
+    
     env = gym.make(env_name)
     continuous = isinstance(env.action_space, gym.spaces.Box)
     ob_dim = env.observation_space.shape[0]
     ac_dim = env.action_space.shape[0] if continuous else env.action_space.n
 
-    experiment_dir = exp_name + '_' + env_name + '_' + time.strftime("%d-%m-%Y_%H-%M-%S")
-    experiment_dir = os.path.join(log_dir, experiment_dir)
-    ### Logger configuration
-    logger = Logz(experiment_dir)
-    args = inspect.getargspec(experiment)[0]
-    locals_ = locals()
-    params = {k: locals_[k] if k in locals_ else None for k in args}
-    logger.save_params(params)
+    logger = Logz(log_dir)
     
-
-
     vero = Agent(continuous, ob_dim, ac_dim, n_layers=2)
     regina = Sensei(vero, continuous, ob_dim, ac_dim,
                     epochs, batch_size,
-                    # num_epochs=1, batch_size=5,
                     learning_rate, epsilon)
 
     init = tf.global_variables_initializer()
-    # gen = generator.__next__()
-    with tf.Session() as sess:
+    with get_session() as sess:
         sess.run(init)
         
         generator = rollouts_generator(sess, vero, env, sample_horizon)
@@ -58,70 +81,37 @@ def experiment(log_dir, exp_name, env_name, num_iterations, sample_horizon,
 
             if i % 10 == 0 or i == num_iterations-1:
                 mean, std = rewards.mean(), rewards.std()
-                
+
+                logger.log_tabular("Iteration", i)
                 logger.log_tabular("AverageReturn", mean)
                 logger.log_tabular("StdReturn", std)
+                logger.log_tabular("MaxReturn", np.max(rewards))
+                logger.log_tabular("MinReturn", np.min(rewards))
                 logger.dump_tabular()
-                print('Iteration {0:3d}: with average rewards {1:5.3f} and std {2:5.2f}'
-                      .format(i, mean, std))
+                # print('Iteration {0:3d}: with average rewards {1:5.3f} and std {2:5.2f}'
+                #       .format(i, mean, std))
 
-        render(sess, vero, env)
-    
+            if i % 100 == 0 or i == num_iterations-1:
+                print('iteration: %d'%i)
+
+        # render(sess, vero, env)
+
 def main():
-    if not(os.path.exists('experiments')):
-        os.makedirs('experiments')
-    experiment('experiments', 'PPO', 'Pendulum-v0', num_iterations=400, sample_horizon=2048,
-               gamma=0.99, lam=0.95, learning_rate=5e-3, epsilon=0.2, epochs=10, batch_size=64)
+    # Leaving everything in a single function for easy later CLI argument parsing
+    experiment_params = dict(
+        exp_name='PPO-00',
+        env_name='Pendulum-v0',
+        num_iterations=400,
+        sample_horizon=2048,
+        # Learning hyperparameters
+        epochs=10, batch_size=64, learning_rate=5e-3,
+        # GAE params
+        gamma=0.99, lam=0.95,
+        # PPO specific hyperparameter, not gonna change this :v
+        epsilon=0.2,
+    )
+    experiment(**experiment_params)
     
     
-
-# def main():
-#     # env = gym.make('MountainCarContinuous-v0')
-#     env = gym.make('Pendrogone-v0')
-#     # env = gym.make('CartPole-v1')
-
-#     continuous = isinstance(env.action_space, gym.spaces.Box)
-#     ob_dim = env.observation_space.shape[0]
-#     ac_dim = env.action_space.shape[0] if continuous else env.action_space.n
-
-#     num_iterations = 400
-#     sample_horizon = 2048
-#     # num_iterations = 1
-#     # sample_horizon = 5
-#     gamma, lam = 0.99, 0.95
-
-#     vero = Agent(continuous, ob_dim, ac_dim, n_layers=2)
-#     regina = Sensei(vero, continuous, ob_dim, ac_dim,
-#                     num_epochs=10, batch_size=64,
-#                     # num_epochs=1, batch_size=5,
-#                     learning_rate=5e-3, epsilon=0.2)
-
-#     init = tf.global_variables_initializer()
-#     # gen = generator.__next__()
-#     with tf.Session() as sess:
-#         sess.run(init)
-        
-#         generator = rollouts_generator(sess, vero, env, sample_horizon)
-
-#         for i in range(num_iterations):
-#             seg = generator.__next__()
-#             add_vtarg_adv(seg, lam, gamma)
-
-#             adv = seg["adv"]
-#             adv = (adv - adv.mean()) / (adv.std() + 1e-8)
-#             adv = adv[:, None]
-
-#             regina.train_samples(sess, seg["ob"], seg["ac"], adv, seg["vtarg"], seg["log_probs"])
-
-#             rewards = np.array(seg["ep_rets"])
-#             # print(rewards)
-
-#             if i % 10 == 0 or i == num_iterations-1:
-#                 print('Iteration {0:3d}: with average rewards {1:5.3f} and std {2:5.2f}'
-#                       .format(i, rewards.mean(), rewards.std()))
-
-#         render(sess, vero, env)
-        
-
 if __name__ == '__main__':
     main()
