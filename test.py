22f234dbe258795bb402473e72a4cc0f60d18f05
Raiszo <E05.sek@gmail.com>
2018-10-27 11:56:57 -0500

    :dagger: testing new environment

--- test.py
+++ test.py
@@ -1,38 +1,59 @@
 import gym
 import tensorflow as tf
 import numpy as np
-from policy import Policy
+from agent import Agent
+from ppo import render, rollouts_generator, Sensei
+
+num_iterations = 2
+sample_horizon = 10
+# Learning hyperparameters
+epochs=2
+batch_size=2
+learning_rate=3e-4
+# GAE params
+gamma=0.99
+lam=0.95
+# PPO specific hyperparameter, not gonna change this :v
+epsilon=0.2
 
 def main():
-    env = gym.make('CartPole-v1')
+    env = gym.make('LunarLanderContinuous-v2')
+    
     continuous = isinstance(env.action_space, gym.spaces.Box)
+    print(continuous)
 
+    ob_dim = env.observation_space.shape[0]
     ac_dim = env.action_space.shape[0] if continuous else env.action_space.n
 
-    horizon = 5
+    veronika = Agent(continuous, ob_dim, ac_dim, n_layers=2)
+    regina = Sensei(veronika, continuous, ob_dim, ac_dim,
+                    epochs, batch_size,
+                    learning_rate, epsilon)
 
-    # Sampled variables
-    ob_no = tf.placeholder(tf.float32, shape=[None, env.observation_space.shape[0]])
-
-    pi = Policy('veronika', ob_no, ac_dim, continuous, n_layers=2)
-
-    
-    ob = env.reset()
-    obs = np.array([ob for _ in range(horizon)])
     
     init = tf.global_variables_initializer()
     with tf.Session() as sess:
         sess.run(init)
-        
-        for i in range(horizon):
-            ac, vpred = pi.act(sess, ob)
-            print(ac)
+        generator = rollouts_generator(sess, veronika, env, sample_horizon)
 
-            obs[i] = ob
+        for i in range(num_iterations):
+            seg = generator.__next__()
+            add_vtarg_adv(seg, lam, gamma)
 
-        print(obs.shape)
-        s_ac, ac = sess.run([pi.sample_action, pi.logits], feed_dict={pi.state: obs})
-        print(s_ac, ac)
+            adv = seg["adv"]
+            adv = (adv - adv.mean()) / (adv.std() + 1e-8)
+            adv = adv[:, None]
+
+            regina.train_samples(sess, seg["ob"], seg["ac"], adv, seg["vtarg"], seg["log_probs"])
+
+            rewards = np.array(seg["ep_rets"])
+            
+            if i % 10 == 0 or i == num_iterations-1:
+                mean, std = rewards.mean(), rewards.std()
+                print('Iteration {0:3d}: with average rewards {1:5.3f} and std {2:5.2f}'
+                      .format(i, mean, std))
+
+        render(sess, veronika, env)
 
 if __name__ == '__main__':
     main()
