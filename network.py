447226287364c3b1cf570e82ec1fb64c48f13f04
Raiszo <E05.sek@gmail.com>
2018-11-23 18:18:44 -0500

    :dagger: need to work with inialization and find why action is < 0

--- network.py
+++ network.py
@@ -4,6 +4,7 @@ def build_mlp(n_layers, input_placeholder, output_size, size=32):
 
     y = input_placeholder
     for i in range(n_layers):
-        y = tf.layers.dense(y, size, activation=tf.tanh, use_bias=True)
+            # y = tf.layers.dense(y, size, activation=tf.tanh, kernel_initializer=tf.constant_initializer(0), use_bias=True)
+            y = tf.layers.dense(y, size, activation=tf.tanh, use_bias=True)
 
     return tf.layers.dense(y, output_size, use_bias=True)
